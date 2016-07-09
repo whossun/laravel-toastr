b148de62f215711381799515ca3dc5718c1e3e8a
whossun <whossun@gmail.com>
2016-07-09 14:31:24 +0800

    add package source files.

---
+++ src/Facades/Toastr.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Whossun\Toastr\Facades;
+
+use Illuminate\Support\Facades\Facade;
+
+class Toastr extends Facade
+{
+    protected static function getFacadeAccessor()
+    {
+        return 'toastr';
+    }
+}
---
+++ src/views/toastr.blade.php
@@ -0,0 +1,4 @@
+<link href="{{asset('packages/whossun/laravel-toastr/css/toastr.min.css')}}" rel="stylesheet" type="text/css" />
+<script src="{{asset('packages/whossun/laravel-toastr/js/jquery.min.js')}}"></script>
+<script src="{{asset('packages/whossun/laravel-toastr/js/toastr.min.js')}}"></script>
+{!! $script !!}
